0386c69afde87d41d4727965368163266250ec9c
Charlie Plate <charlie@graphiteconnect.com>
2023-11-21 15:50:00 -0500

    Add Router

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 "use strict";
-const tags = ["a", "div", "h1", "p", "button"];
+const tags = ["a", "div", "h1", "p", "button", "h2"];
+let scopes = {};
+window.scopes = scopes;
 for (const tag of tags) {
     window[tag] = (...children) => {
-        const newTag = new HtmlTag(tag, ...children);
+        const newTag = new HtmlTag(tag, ...children.map((child) => {
+            if (typeof child === "string") {
+                const t = new HtmlTag(tag, []);
+                t.text = child;
+                return t;
+            }
+            return child;
+        }));
         newTag.create();
         return newTag;
     };
@@ -19,6 +28,7 @@ class HtmlTag {
     reactive;
     children;
     parent = null;
+    text;
     type;
     constructor(type, ...children) {
         this.type = type;
@@ -26,7 +36,24 @@ class HtmlTag {
         this.reactive = false;
         for (const child of children) {
             if (typeof child === "function") {
-                this.children.push(...child());
+                const result = child();
+                if (Array.isArray(result)) {
+                    for (const r of result) {
+                        if (typeof r === "string") {
+                            const t = new HtmlTag(type, []);
+                            t.text = r;
+                            this.children.push(t);
+                        }
+                        else {
+                            this.children.push(r);
+                        }
+                    }
+                }
+            }
+            else if (typeof child === "string") {
+                const t = new HtmlTag(type, []);
+                t.text = child;
+                this.children.push(t);
             }
             else {
                 this.children.push(child);
@@ -36,13 +63,13 @@ class HtmlTag {
     create() {
         this.element = document.createElement(this.type);
         for (const child of this.children) {
-            if (typeof child === "string") {
-                this.element.appendChild(document.createTextNode(child));
+            if (child.text) {
+                this.element.appendChild(document.createTextNode(child.text));
             }
             else {
                 this.element.appendChild(child.element);
-                child.parent = this;
             }
+            child.parent = this;
         }
     }
     destroy() {
@@ -65,9 +92,11 @@ class HtmlTag {
 }
 class ReactiveHtmlTag extends HtmlTag {
     tagMethod;
+    scopes;
     constructor(type, ...children) {
         super(type, ...children);
         this.reactive = true;
+        this.scopes = [];
     }
     static create(tagMethod) {
         const tag = tagMethod();
@@ -75,55 +104,139 @@ class ReactiveHtmlTag extends HtmlTag {
         return tag;
     }
     react() {
-        const newElement = ReactiveHtmlTag.create(this.tagMethod);
-        while (this.element.firstChild) {
-            this.element.removeChild(this.element.firstChild);
+        for (const child of this.children) {
+            if (child instanceof ReactiveHtmlTag) {
+                child.clearChildScopes();
+            }
         }
+        const newElement = ReactiveHtmlTag.create(this.tagMethod);
+        this.children = newElement.children;
         this.element.replaceChildren(...newElement.children.map((child) => {
             if (child instanceof ReactiveHtmlTag) {
                 const newChild = ReactiveHtmlTag.create(() => child);
                 newChild.parent = this;
                 return newChild.element;
             }
-            else if (typeof child === "string") {
-                return document.createTextNode(child);
+            else if (child.text) {
+                return document.createTextNode(child.text);
             }
             else {
                 return child.element;
             }
         }));
     }
-    $handle(handlerType, handler, react = true) {
+    scope(scope) {
+        if (typeof scope === "string") {
+            scope = [scope];
+        }
+        this.scopes.push(...scope);
+        for (const scopeName of scope) {
+            if (!scopes[scopeName]) {
+                scopes[scopeName] = new Set();
+            }
+            scopes[scopeName].add(this);
+        }
+        return this;
+    }
+    clearChildScopes() {
+        for (const child of this.children) {
+            if (child instanceof ReactiveHtmlTag) {
+                child.clearChildScopes();
+            }
+        }
+        for (const scope of this.scopes) {
+            scopes[scope].delete(this);
+        }
+    }
+    $handle(handlerType, handler) {
         const cb = () => {
             handler(this);
-            if (!react) {
-                return this;
-            }
             let lastReactParent = this;
-            let next = this.parent;
+            let next = this;
             while (next.parent !== null) {
                 if (next.reactive) {
-                    lastReactParent = lastReactParent.parent;
+                    lastReactParent = next;
                 }
                 next = next.parent;
             }
-            lastReactParent.react();
+            if (lastReactParent !== this) {
+                lastReactParent.react();
+            }
+            else {
+                console.error("No parent to react");
+            }
+        };
+        this.element.addEventListener(handlerType, cb);
+        return this;
+    }
+    $$handle(emitTo, handlerType, handler) {
+        const cb = () => {
+            handler(this);
+            for (const scope of emitTo) {
+                for (const element of scopes[scope]) {
+                    element.react();
+                }
+            }
         };
         this.element.addEventListener(handlerType, cb);
         return this;
     }
 }
-const parentDiv = document.getElementById("main");
+class Router {
+    routes;
+    mounted;
+    constructor(routes) {
+        this.routes = routes;
+        this.mounted = routes["/"]
+            ? routes["/"]()
+            : routes["404"]
+                ? routes["404"]()
+                : div("404 Not Found");
+        const hashChangeEvent = new HashChangeEvent("hashchange", {
+            newURL: "/",
+            oldURL: "",
+        });
+        window.dispatchEvent(hashChangeEvent);
+        window.addEventListener("hashchange", () => {
+            this.route();
+        });
+    }
+    route() {
+        const path = window.location.hash.slice(1);
+        scopes = {};
+        if (this.routes[path]) {
+            this.mounted.destroy();
+            this.mounted = this.routes[path]();
+            this.mounted.create();
+        }
+        else {
+            this.mounted.destroy();
+            this.mounted = this.routes["404"]
+                ? this.routes["404"]()
+                : div("404 Not Found");
+            this.mounted.create();
+        }
+        document.getElementById("main").appendChild(this.mounted.element);
+    }
+    mount(element) {
+        this.mounted = element;
+    }
+}
+const withRouter = (routes) => {
+    const router = new Router(routes);
+    return router.mounted;
+};
 let count = 0;
-const result = div($div(() => [
-    `Count is ${count}`,
-    $button(() => ["Click me!"])
-        .$handle("click", () => {
-        count++;
-    })
-        .class(count % 2 == 0 ? "green" : "red"),
-    $div(() => [
-        $button(() => ["Reset Button"]).$handle("click", () => (count = 0)),
-    ]),
-]).attr("id", "divWithButton"), a("This is the Text").attr("href", "#/helloWorld")).attr("id", "rootDiv");
-parentDiv.appendChild(result.element);
+const results = withRouter({
+    "/": () => div(div(() => {
+        return [
+            $div(() => [`The Current Count is ${count}`]).scope("count"),
+            $button(() => ["Click Me"]).$$handle(["count"], "click", () => {
+                count++;
+            }),
+        ];
+    }), div(a("click me to go to test").attr("href", "#/test")).class("red"), $div(() => [`Distant Count ${count}`]).scope("count")),
+    "/test": () => div("test", a("Nav to home").attr("href", "#/"), a("Go to something unknown").attr("href", "#/unknown"), `${count}`).class("flex"),
+    "404": () => div("This is what it would be like if there was a custom 404 page", div(a("Go home").attr("href", "/"))),
+});
+document.getElementById("main").appendChild(results.element);
--- index.ts
+++ index.ts
@@ -1,25 +1,51 @@
-declare function div(...children: (HtmlTag | string)[]): HtmlTag;
-declare function a(...children: (HtmlTag | string)[]): HtmlTag;
-declare function h1(...children: (HtmlTag | string)[]): HtmlTag;
-declare function p(...children: (HtmlTag | string)[]): HtmlTag;
-declare function button(...children: (HtmlTag | string)[]): HtmlTag;
+declare function div(
+  ...children: (HtmlTag | string | (() => (HtmlTag | string)[]))[]
+): HtmlTag;
+declare function a(
+  ...children: (HtmlTag | string | (() => (HtmlTag | string)[]))[]
+): HtmlTag;
+declare function h1(
+  ...children: (HtmlTag | string | (() => (HtmlTag | string)[]))[]
+): HtmlTag;
+declare function p(
+  ...children: (HtmlTag | string | (() => (HtmlTag | string)[]))[]
+): HtmlTag;
+declare function button(
+  ...children: (HtmlTag | string | (() => (HtmlTag | string)[]))[]
+): HtmlTag;
 declare function $div(children: () => (HtmlTag | string)[]): ReactiveHtmlTag;
 declare function $a(children: () => (HtmlTag | string)[]): ReactiveHtmlTag;
 declare function $h1(children: () => (HtmlTag | string)[]): ReactiveHtmlTag;
 declare function $p(children: () => (HtmlTag | string)[]): ReactiveHtmlTag;
 declare function $button(children: () => (HtmlTag | string)[]): ReactiveHtmlTag;
 
-type Tag = "a" | "div" | "h1" | "p" | "button";
+type Tag = "a" | "div" | "h1" | "p" | "button" | "h2" | "simpleText";
 type Handlers = { [handler: string]: (event: Event) => void };
 
-const tags: Tag[] = ["a", "div", "h1", "p", "button"];
+const tags: Tag[] = ["a", "div", "h1", "p", "button", "h2"];
+
+let scopes: Record<string, Set<ReactiveHtmlTag>> = {};
+(window as any).scopes = scopes;
 
 for (const tag of tags) {
-  (window as any)[tag] = (...children: (HtmlTag | string)[]) => {
-    const newTag = new HtmlTag(tag, ...children);
+  (window as any)[tag] = (
+    ...children: (HtmlTag | string | (() => HtmlTag | string))[]
+  ) => {
+    const newTag = new HtmlTag(
+      tag,
+      ...children.map((child) => {
+        if (typeof child === "string") {
+          const t = new HtmlTag(tag, [] as any);
+          t.text = child;
+          return t;
+        }
+        return child;
+      }),
+    );
     newTag.create();
     return newTag;
   };
+
   (window as any)[`$${tag}`] = (...children: (HtmlTag | string)[]) => {
     return ReactiveHtmlTag.create(() => {
       const newTag = new ReactiveHtmlTag(tag, ...children);
@@ -32,9 +58,10 @@ for (const tag of tags) {
 class HtmlTag {
   public element!: HTMLElement;
   public reactive: boolean;
-  public children: Array<HtmlTag | ReactiveHtmlTag | string>;
+  public children: Array<HtmlTag | ReactiveHtmlTag>;
   public parent: HtmlTag | ReactiveHtmlTag | null = null;
-  private type: Tag;
+  public text?: string;
+  public type: Tag;
 
   public constructor(
     type: Tag,
@@ -45,7 +72,22 @@ class HtmlTag {
     this.reactive = false;
     for (const child of children) {
       if (typeof child === "function") {
-        this.children.push(...child());
+        const result = child();
+        if (Array.isArray(result)) {
+          for (const r of result) {
+            if (typeof r === "string") {
+              const t = new HtmlTag(type, [] as any);
+              t.text = r;
+              this.children.push(t);
+            } else {
+              this.children.push(r);
+            }
+          }
+        }
+      } else if (typeof child === "string") {
+        const t = new HtmlTag(type, [] as any);
+        t.text = child;
+        this.children.push(t);
       } else {
         this.children.push(child);
       }
@@ -56,12 +98,12 @@ class HtmlTag {
     this.element = document.createElement(this.type);
 
     for (const child of this.children) {
-      if (typeof child === "string") {
-        this.element.appendChild(document.createTextNode(child));
+      if (child.text) {
+        this.element.appendChild(document.createTextNode(child.text!));
       } else {
         this.element.appendChild(child.element);
-        child.parent = this;
       }
+      child.parent = this;
     }
   }
 
@@ -89,10 +131,12 @@ class HtmlTag {
 
 class ReactiveHtmlTag extends HtmlTag {
   private tagMethod!: () => ReactiveHtmlTag;
+  private scopes: string[];
 
   constructor(type: Tag, ...children: Array<HtmlTag | string>) {
     super(type, ...children);
     this.reactive = true;
+    this.scopes = [];
   }
 
   public static create(tagMethod: () => ReactiveHtmlTag) {
@@ -102,18 +146,24 @@ class ReactiveHtmlTag extends HtmlTag {
   }
 
   public react() {
-    const newElement = ReactiveHtmlTag.create(this.tagMethod);
-    while (this.element.firstChild) {
-      this.element.removeChild(this.element.firstChild);
+    for (const child of this.children) {
+      if (child instanceof ReactiveHtmlTag) {
+        child.clearChildScopes();
+      }
     }
+
+    const newElement = ReactiveHtmlTag.create(this.tagMethod);
+
+    this.children = newElement.children;
+
     this.element.replaceChildren(
       ...newElement.children.map((child) => {
         if (child instanceof ReactiveHtmlTag) {
           const newChild = ReactiveHtmlTag.create(() => child);
           newChild.parent = this;
           return newChild.element;
-        } else if (typeof child === "string") {
-          return document.createTextNode(child);
+        } else if (child.text) {
+          return document.createTextNode(child.text!);
         } else {
           return child.element;
         }
@@ -121,48 +171,157 @@ class ReactiveHtmlTag extends HtmlTag {
     );
   }
 
+  public scope(scope: string | string[]) {
+    if (typeof scope === "string") {
+      scope = [scope];
+    }
+    this.scopes.push(...scope);
+    for (const scopeName of scope) {
+      if (!scopes[scopeName]) {
+        scopes[scopeName] = new Set();
+      }
+      scopes[scopeName].add(this);
+    }
+    return this;
+  }
+
+  public clearChildScopes() {
+    for (const child of this.children) {
+      if (child instanceof ReactiveHtmlTag) {
+        child.clearChildScopes();
+      }
+    }
+    for (const scope of this.scopes) {
+      scopes[scope].delete(this);
+    }
+  }
+
   public $handle(
     handlerType: string,
     handler: (element: ReactiveHtmlTag) => void,
-    react: boolean = true,
   ) {
     const cb = () => {
       handler(this);
-      if (!react) {
-        return this;
-      }
       let lastReactParent: HtmlTag | ReactiveHtmlTag = this;
-      let next = this.parent;
+      let next: HtmlTag | ReactiveHtmlTag = this;
       while (next.parent !== null) {
         if (next.reactive) {
-          lastReactParent = lastReactParent.parent;
+          lastReactParent = next!;
         }
         next = next.parent;
       }
-      lastReactParent.react();
+      if (lastReactParent !== this) {
+        (lastReactParent as ReactiveHtmlTag).react();
+      } else {
+        console.error("No parent to react");
+      }
     };
 
     this.element.addEventListener(handlerType, cb);
 
     return this;
   }
+
+  public $$handle(
+    emitTo: string[],
+    handlerType: string,
+    handler: (element: ReactiveHtmlTag) => void,
+  ) {
+    const cb = () => {
+      handler(this);
+      for (const scope of emitTo) {
+        for (const element of scopes[scope]) {
+          element.react();
+        }
+      }
+    };
+
+    this.element.addEventListener(handlerType, cb);
+    return this;
+  }
 }
 
-const parentDiv = document.getElementById("main");
-let count = 0;
-const result = div(
-  $div(() => [
-    `Count is ${count}`,
-    $button(() => ["Click me!"])
-      .$handle("click", () => {
-        count++;
-      })
-      .class(count % 2 == 0 ? "green" : "red"),
-    $div(() => [
-      $button(() => ["Reset Button"]).$handle("click", () => (count = 0)),
-    ]),
-  ]).attr("id", "divWithButton"),
-  a("This is the Text").attr("href", "#/helloWorld"),
-).attr("id", "rootDiv");
+class Router {
+  public routes: Record<string, () => HtmlTag | ReactiveHtmlTag>;
+  public mounted: HtmlTag | ReactiveHtmlTag;
 
-parentDiv!.appendChild(result.element);
+  constructor(routes: Record<string, () => HtmlTag | ReactiveHtmlTag>) {
+    this.routes = routes;
+    this.mounted = routes["/"]
+      ? routes["/"]()
+      : routes["404"]
+        ? routes["404"]()
+        : div("404 Not Found");
+
+    const hashChangeEvent = new HashChangeEvent("hashchange", {
+      newURL: "/",
+      oldURL: "",
+    });
+
+    window.dispatchEvent(hashChangeEvent);
+    window.addEventListener("hashchange", () => {
+      this.route();
+    });
+  }
+
+  public route() {
+    const path = window.location.hash.slice(1);
+    scopes = {};
+
+    if (this.routes[path]) {
+      this.mounted.destroy();
+      this.mounted = this.routes[path]();
+      this.mounted.create();
+    } else {
+      this.mounted.destroy();
+      this.mounted = this.routes["404"]
+        ? this.routes["404"]()
+        : div("404 Not Found");
+      this.mounted.create();
+    }
+
+    document.getElementById("main")!.appendChild(this.mounted.element);
+  }
+
+  public mount(element: HtmlTag | ReactiveHtmlTag) {
+    this.mounted = element;
+  }
+}
+
+const withRouter = (
+  routes: Record<string, () => HtmlTag | ReactiveHtmlTag>,
+): HtmlTag | ReactiveHtmlTag => {
+  const router = new Router(routes);
+  return router.mounted;
+};
+
+let count = 0;
+const results = withRouter({
+  "/": () =>
+    div(
+      div(() => {
+        return [
+          $div(() => [`The Current Count is ${count}`]).scope("count"),
+          $button(() => ["Click Me"]).$$handle(["count"], "click", () => {
+            count++;
+          }),
+        ];
+      }),
+      div(a("click me to go to test").attr("href", "#/test")).class("red"),
+      $div(() => [`Distant Count ${count}`]).scope("count"),
+    ),
+  "/test": () =>
+    div(
+      "test",
+      a("Nav to home").attr("href", "#/"),
+      a("Go to something unknown").attr("href", "#/unknown"),
+      `${count}`,
+    ).class("flex"),
+  "404": () =>
+    div(
+      "This is what it would be like if there was a custom 404 page",
+      div(a("Go home").attr("href", "/")),
+    ),
+});
+
+document.getElementById("main")!.appendChild(results.element);
